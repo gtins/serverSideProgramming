0729d4ee9c28648b40ccbc01664db789ffee704d
gtins <111321003+gtins@users.noreply.github.com>
2023-09-11 14:48:55 -0300

    Delete pasta_tres directory

--- pasta_tres/definicoes_3.js
+++
@@ -1,8 +0,0 @@
-const prompt = require('prompt-sync')();
-
-function defineAno(){
-    const ano = prompt('Insira um ano: ')
-    return ano;
-}
-
-module.exports = {defineAno};
--- pasta_tres/resultado_ano.js
+++
@@ -1,7 +0,0 @@
-const resultado = require('./processamento_3');
-const input_ano = require('./definicoes_3');
-
-var ano = input_ano.defineAno();
-var resultadoAno = resultado.verificaAno(ano);
-
-console.log(resultadoAno);
